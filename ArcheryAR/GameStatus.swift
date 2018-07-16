a775a175d1716bd8f12d3e0a2bdac3d775b6f513
Valmir Jr <valmirmassoni@gmail.com>
2018-07-16 17:06:40 -0300

    Adding target

---
+++ ArcheryAR/GameStatus.swift
@@ -0,0 +1,15 @@
+//
+//  GameStatus.swift
+//  ArcheryAR
+//
+//  Created by Valmir Junior on 16/07/18.
+//  Copyright © 2018 Valmir Junior. All rights reserved.
+//
+
+import Foundation
+
+enum GameStatus: String {
+    case waitingForPlane
+    case waitingForTarget
+    case targetInGame
+}
--- ArcheryAR/ViewController.swift
+++ ArcheryAR/ViewController.swift
@@ -10,34 +10,28 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    // MARK: - ARKit / ARSCNView
+    let session = ARSession()
+    var sessionConfig = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        self.setupScene()
+        self.setupARDetection()
+        self.setupDebugger()
+        self.addTapGestureToSceneView()
         
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        self.session.run(sessionConfig)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -49,19 +43,136 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
+    
+    fileprivate func setupScene() {
+        // set up sceneView
+        sceneView.delegate = self
+        sceneView.session = session
+        sceneView.antialiasingMode = .multisampling4X
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+        
+        sceneView.preferredFramesPerSecond = 60
+        sceneView.contentScaleFactor = 1.3
+        //sceneView.showsStatistics = true
+        
+    
+        if let camera = sceneView.pointOfView?.camera {
+            camera.wantsHDR = true
+            camera.wantsExposureAdaptation = true
+            camera.exposureOffset = -1
+            camera.minimumExposure = -1
+        }
+        
+        
+    }
+    
+    fileprivate func setupARDetection() {
+        if ARConfiguration.isSupported {
+            //starts plane detection
+            self.sessionConfig.planeDetection = [.vertical, .horizontal]
+            
+            self.session.delegate = self
+            self.session.run(sessionConfig)
+            
+        } else {
+            self.performSegue(withIdentifier: "arkitNotSupported", sender: nil)
+        }
+    }
+    
+    fileprivate func setupDebugger() {
+        self.sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
+    }
+    
+    fileprivate func addTapGestureToSceneView() {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self,
+            action: #selector(ViewController.addTargetToPlane(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func addTargetToPlane(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation, types: .existingPlaneUsingExtent)
+        
+        guard let hitTestResult = hitTestResults.first else { return }
+        let translation = hitTestResult.worldTransform.columns.3
+        let x = translation.x
+        let y = translation.y
+        let z = translation.z
+        
+        guard let targetScene = SCNScene(named: "art.scnassets/target.scn"),
+            let targetNode = targetScene.rootNode.childNode(withName: "target", recursively: false)
+            else { return }
+        
+        
+        targetNode.position = SCNVector3(x, y, z)
+        sceneView.scene.rootNode.addChildNode(targetNode)
+    }
+    
+    
+}
 
+extension ViewController: ARSCNViewDelegate {
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
+     return node
+     }
+     */
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        // 1
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        let plane = SCNPlane(width: width, height: height)
+        
+        // 3
+        plane.materials.first?.diffuse.contents = UIColor.grayTransparent
+        
+        // 4
+        let planeNode = SCNNode(geometry: plane)
+        
+        // 5
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // 6
+        node.addChildNode(planeNode)
+        
     }
-*/
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // guarantee that gets a plane
+        guard let planeAnchor = anchor as?  ARPlaneAnchor,
+            let planeNode = node.childNodes.first,
+            let plane = planeNode.geometry as? SCNPlane
+            else { return }
+        
+        // 2
+        let width = CGFloat(planeAnchor.extent.x)
+        let height = CGFloat(planeAnchor.extent.z)
+        plane.width = width
+        plane.height = height
+        
+        // 3
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode.position = SCNVector3(x, y, z)
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
@@ -77,4 +188,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+}
+
+extension ViewController: ARSessionDelegate {
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        
+    }
+    
 }
